01a2557f1fa8dbb55b580fa045625f6372cf705f
Farabe1911 <tasnimul_farabe@hotmail.com>
2024-07-27 13:25:05 -0500

    done

--- mail.php
+++ mail.php
@@ -14,22 +14,22 @@
 	$message = $_POST['message'];
 	
 	if( empty($name) ){
-		$data['error'] = 'Please enter your name.';
+		$data['error0'] = 'Please enter your name.';
 	}else if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
-		$data['error'] = 'Please enter a valid email address.';
+		$data['error1'] = 'Please enter a valid email address.';
 	}else if( empty($message) ){
-		$data['error'] = 'The message field is required!';
+		$data['error2'] = 'The message field is required!';
 	}else if( empty($phone) ){
-		$data['error'] = 'Please enter your phone number.';
+		$data['error3'] = 'Please enter your phone number.';
 	}else if ( empty($website) ){
-		$data['error'] = 'Please enter your website.';
+		$data['error4'] = 'Please enter your website.';
 	}else{
 		
 		$formcontent="From: $name\nPhone: $phone\nWebsite: $website\nEmail: $email\nMessage: $message";
 		
 		
 		//Place your Email Here
-		$recipient = "your_email@domain.com";
+		$recipient = "farabetasnimul@gmail.com";
 		
 		$mailheader = "From: $email \r\n";
 		
